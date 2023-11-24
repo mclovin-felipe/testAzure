e6187a39183452577113a82a6b10949b4489cdfb
Felipe Ignacio Ponce Canales <66095148+mclovin-felipe@users.noreply.github.com>
2023-11-24 10:56:35 -0300

    fix provider

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -3,11 +3,12 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import AzureADProvider from "next-auth/providers/azure-ad";
 export const options = {
   providers: [
-      AzureADProvider({
-        clientId: process.env.NEXT_PUBLIC_AZURE_AD_CLIENT_ID,
-        clientSecret: process.env.NEXT_PUBLIC_AZURE_AD_CLIENT_SECRET,
-        tenantId: process.env.NEXT_PUBLIC_AZURE_AD_TENANT_ID,
-      }),
+    AzureADProvider({
+      clientId: process.env.NEXT_PUBLIC_AZURE_AD_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_AZURE_AD_CLIENT_SECRET,
+      tenantId: process.env.NEXT_PUBLIC_AZURE_AD_TENANT_ID,
+    }),
+
     CredentialsProvider({
       id: "creddentials",
       name: "Donaciones",
@@ -19,7 +20,7 @@ export const options = {
         },
         password: { label: "Password", type: "passowrd" },
       },
-      
+
 
       async authorize(credentials, req) {
         const user = {
@@ -45,4 +46,13 @@ export const options = {
       secret: process.env.NEXTAUTH_SECRET,
     }),
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      // IMPORTANT: Persist the access_token to the token right after sign in
+      if (account) {
+        token.idToken = account.id_token;
+      }
+      return token;
+    },
+  },
 };
