c34996dd7791744d3ca6837fb1dc813d763eb6a3
Felipe Ignacio Ponce Canales <66095148+mclovin-felipe@users.noreply.github.com>
2023-11-24 11:07:58 -0300

    fix provider

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -10,44 +10,12 @@ export const options = {
       authorization: { params: { scope: "openid profile user.Read email" } },
     }),
 
-    CredentialsProvider({
-      id: "creddentials",
-      name: "Donaciones",
-      credentials: {
-        username: {
-          label: "Usuario",
-          type: "text",
-          placeholder: "Nombre de usuario",
-        },
-        password: { label: "Password", type: "passowrd" },
-      },
 
-
-      async authorize(credentials, req) {
-        const user = {
-          id: "1",
-          name: "Prueba",
-          password: "123",
-          email: "jsmith@example.com",
-        };
-
-        if (
-          user.name === credentials.username &&
-          user.password === credentials.password
-        ) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
-      },
-      secret: process.env.NEXTAUTH_SECRET,
-    }),
   ],
   callbacks: {
+    async signIn() {
+      return true;
+    },
     async jwt({ token, account }) {
       // IMPORTANT: Persist the access_token to the token right after sign in
       if (account) {
