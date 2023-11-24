4e337a8e6c712cee1360e00cf364e368f264ded2
Vicente Berroeta Palma <vicente.berroeta@falp.org>
2023-11-24 11:16:12 -0300

    Cambio en options

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -9,46 +9,10 @@ export const options = {
       tenantId: process.env.NEXT_PUBLIC_AZURE_AD_TENANT_ID,
       authorization: { params: { scope: "openid profile user.Read email" } },
     }),
-
-    CredentialsProvider({
-      id: "creddentials",
-      name: "Donaciones",
-      credentials: {
-        username: {
-          label: "Usuario",
-          type: "text",
-          placeholder: "Nombre de usuario",
-        },
-        password: { label: "Password", type: "passowrd" },
-      },
-
-
-      async authorize(credentials, req) {
-        const user = {
-          id: "1",
-          name: "Prueba",
-          password: "123",
-          email: "jsmith@example.com",
-        };
-
-        if (
-          user.name === credentials.username &&
-          user.password === credentials.password
-        ) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
-      },
-      secret: process.env.NEXTAUTH_SECRET,
-    }),
   ],
   callbacks: {
-    async signIn() {
+    async signIn(user, account, profile) {
+      console.log("Sign In:", user, account, profile);
       return true;
     },
     async jwt({ token, account }) {
