583c7573b7fe28dd10243dc92625c05fa9807b72
Felipe Ignacio Ponce Canales <66095148+mclovin-felipe@users.noreply.github.com>
2023-11-27 09:53:01 -0300

    Revert "Cambio en options"
    This reverts commit 6e5a2218d4eb026364a8ba5f19b3b13520e4b5ae, reversing
    changes made to 8063ac5fcf9c54d38c64036d272b323b306a9c3a.

---
+++ src/app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,34 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import AzureADProvider from "next-auth/providers/azure-ad";
+export const options = {
+  providers: [
+    AzureADProvider({
+      clientId: process.env.NEXT_PUBLIC_AZURE_AD_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_AZURE_AD_CLIENT_SECRET,
+      tenantId: process.env.NEXT_PUBLIC_AZURE_AD_TENANT_ID,
+    }),
+  ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      console.log("Sign In:", user, account, profile);
+      return true;
+    },
+    async jwt({ token, account }) {
+      console.log("JWT:", token, account);
+      // IMPORTANT: Persist the access_token to the token right after sign in
+      if (account) {
+        token.accessToken = account.access_token
+      }
+      return token;
+    },
+    async redirect(url, baseUrl) {
+      console.log("Redirect:", url, baseUrl);
+      return baseUrl;
+    },
+    async session(session, user) {
+      console.log("Session:", session, user);
+      return session;
+    },
+  },
+};
