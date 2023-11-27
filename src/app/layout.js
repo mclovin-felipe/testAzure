e504b6e97503aec9f4cc3c31585fe5e0e1fc2dd5
Vicente Berroeta Palma <vicente.berroeta@falp.org>
2023-11-27 11:30:26 -0300

    Cambio layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,5 @@
 "use client"
 import { SessionProvider } from "next-auth/react";
-export const metadata = {
-  title: 'Portal Notificaciones',
-  description: 'Creado para facilitar el envió de notificaciones',
-}
 
 export default function RootLayout({ children }) {
   return (
