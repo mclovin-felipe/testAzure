83c2fceb771a87916690cf3e8e9a8e10d93dc2f6
Felipe Ignacio Ponce Canales <66095148+mclovin-felipe@users.noreply.github.com>
2023-11-28 16:37:43 -0300

    prueba

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth/next";
 import AzureADProvider from "next-auth/providers/azure-ad";
-
 const authOptions = {
   providers: [
     AzureADProvider({
@@ -8,16 +7,18 @@ const authOptions = {
       clientSecret: process.env.NEXT_PUBLIC_AZURE_AD_CLIENT_SECRET,
       tenantId: process.env.NEXT_PUBLIC_AZURE_AD_TENANT_ID,
     }),
+
   ],
-  // pages: {
-  //   signIn: '/auth/signin',
-  // },
+  pages: {
+    signIn: "/auth/signin",
+  },
   session: {
     strategy: "jwt",
+
   },
   secret: process.env.NEXTAUTH_SECRET,
 
 }
 
 const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST, };
+export { handler as GET, handler as POST };
