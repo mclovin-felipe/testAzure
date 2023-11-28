9991c1db1735933b0346fc883a9813de0bab526d
Vicente Berroeta Palma <vicente.berroeta@falp.org>
2023-11-28 18:32:34 -0300

    Cambio login

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -16,12 +16,6 @@ const authOptions = {
     strategy: "jwt",
 
   },
-  callbacks: {
-    async redirect(url, baseUrl) {
-      console.log(`Redirecting to: ${url}`);
-      return url;
-    },
-  },
   secret: process.env.NEXTAUTH_SECRET,
 
 }
