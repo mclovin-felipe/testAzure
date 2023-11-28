f0c547d82e2cb46c8d90d6b5cd92d48c09f25bc0
Vicente Berroeta Palma <vicente.berroeta@falp.org>
2023-11-28 17:10:30 -0300

    Cambio login

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,7 @@
-export { default } from "next-auth/middleware"
-export const config = { matcher: ['/','/pages'] };
-// export const config = { matcher: ["/nda"] }
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/auth/signin",
+  },
+});
