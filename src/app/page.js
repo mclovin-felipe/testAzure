d333d9b0c704be89156d041f2b626a819606b4f6
Vicente Berroeta Palma <vicente.berroeta@falp.org>
2023-11-28 18:47:28 -0300

    Cambio login

--- src/app/page.js
+++ src/app/page.js
@@ -1,44 +1,64 @@
-'use client'
-import { Box, Button, Fade, Stack, Typography } from "@mui/material";
-import { useSession, signOut, signIn } from "next-auth/react";
-import Image from "next/image";
-import Collapse from '@mui/material/Collapse';
-import { useState } from "react";
-import Home from '@/app/pages/home/page'
-export default function HomePage() {
-  const { data, session } = useSession();
-  const [open, setOpen] = useState(false);
-  if (data && data.user) {
-    console.log(data)
-    return (
-      <main>
-        <Home></Home>
-      </main>
-    )
-  }else{
-    return (
-      <Box
-        height={"91.8vh"}
-        width={"100%"}
-        display={"flex"}
-        justifyContent={"center"}
-        bgcolor={'primary.main'}
-        alignItems={"center"}
-  
-      >
-        <Stack boxShadow={12} direction={'row'} height={"35vh"} >
-          <Box flexDirection={'column'} borderRadius={3} width={400} bgcolor={'white'} height={"100%"} display={"flex"} justifyContent={"center"} alignItems={"center"}
-          >
-  
-            <Button color={"secondary"} sx={{ borderRadius: 1000, color: 'black', backgroundColor: 'secondary.main' }} variant="contained" onClick={() => signIn("azure-ad")}>Ingresar</Button>
-  
-          </Box>
-  
-  
-        </Stack>
-  
-      </Box>
-    )
-  }
- 
-}
+"use client"
+import React, { useState } from 'react';
+import { Container, Grid, Typography } from '@mui/material';
+import MenuLateral from '@/app/componets/menuLateral';
+import Formulario from '@/app/componets/formulario';
+import HomeContent from '@/app/componets/homeContent';
+import HistorialNotificaciones from '@/app/componets/historalNotificaciones'
+
+const Home = () => {
+  const camposPorTopico = [
+    { key: 0, title:"Título", value:"titulo", label: 'Titulo de la notificación' },
+    { key: 1, title:"Mensaje", value:"mensaje", label: 'Cuerpo de la notificación' },
+    { key: 2, title:"URL de la imagen", value:"imageUrl", label: 'Example: https://www.institutoncologicofalp.cl/wp-content/uploads/2023/03/LOGO-FALP-ok.png', optional:true },
+    { key: 3, title:"JSON", value:"json", label: 'Example: "data":"jasonData","data":"jasonData","data":"jasonData"', optional:true},
+
+  ];
+
+    const [selectedOption, setSelectedOption] = useState(null);
+    const renderContent = () => {
+        switch (selectedOption) {
+          case 1:
+            return (
+              <Container sx={{display:"flex", flexDirection:"column", alignItems:"center", alignContent:"center"}}>
+                <Typography variant="h4" gutterBottom>
+                  Historial de notificaciones
+                </Typography>
+                <HistorialNotificaciones/>
+              </Container>
+              )
+          case 2:
+            return <div>hola2</div>;
+        case "0_0":
+            return (
+            <Container sx={{display:"flex", flexDirection:"column", alignItems:"center", alignContent:"center"}}>
+              <Typography variant="h4" gutterBottom>
+                Enviar notificación a todos los dispositivos
+              </Typography>
+              <Formulario campos={camposPorTopico} selectedOption={selectedOption}/>
+          </Container>
+          );
+          default:
+            return (
+              <Container sx={{display:"flex", flexDirection:"column", alignItems:"center", alignContent:"center"}}>
+                <HomeContent/>
+              </Container>
+            );
+        }
+      };
+  return (
+    <React.Fragment>
+      <Grid container spacing={2} sx={{alignContent:"center", alignItems:"center"}}>
+        <Grid item md={2}>
+          <MenuLateral handleMenuClick={setSelectedOption}/>
+        </Grid>
+        <Grid item md={10} sx={{marginTop:"5rem"}} >
+        {renderContent()}
+        </Grid>
+      </Grid>
+   </React.Fragment>
+    
+  );
+};
+
+export default Home;
