4d22b3f793c2f40c5f48cac547c90f283c8c38bd
Vicente Berroeta Palma <vicente.berroeta@falp.org>
2023-11-28 18:12:13 -0300

    Cambio login

--- src/app/page.js
+++ src/app/page.js
@@ -4,37 +4,40 @@ import { useSession, signOut, signIn } from "next-auth/react";
 import Image from "next/image";
 import Collapse from '@mui/material/Collapse';
 import { useState } from "react";
+import Home from '@/app/pages/home/page'
 export default function Home() {
   const { data, session } = useSession();
   const [open, setOpen] = useState(false);
   if (data && data.user) {
     return (
       <main>
-        login
+        <Home></Home>
       </main>
     )
+  }else{
+    return (
+      <Box
+        height={"91.8vh"}
+        width={"100%"}
+        display={"flex"}
+        justifyContent={"center"}
+        bgcolor={'primary.main'}
+        alignItems={"center"}
+  
+      >
+        <Stack boxShadow={12} direction={'row'} height={"35vh"} >
+          <Box flexDirection={'column'} borderRadius={3} width={400} bgcolor={'white'} height={"100%"} display={"flex"} justifyContent={"center"} alignItems={"center"}
+          >
+  
+            <Button color={"secondary"} sx={{ borderRadius: 1000, color: 'black', backgroundColor: 'secondary.main' }} variant="contained" onClick={() => signIn("azure-ad")}>Ingresar</Button>
+  
+          </Box>
+  
+  
+        </Stack>
+  
+      </Box>
+    )
   }
-  return (
-    <Box
-      height={"91.8vh"}
-      width={"100%"}
-      display={"flex"}
-      justifyContent={"center"}
-      bgcolor={'primary.main'}
-      alignItems={"center"}
-
-    >
-      <Stack boxShadow={12} direction={'row'} height={"35vh"} >
-        <Box flexDirection={'column'} borderRadius={3} width={400} bgcolor={'white'} height={"100%"} display={"flex"} justifyContent={"center"} alignItems={"center"}
-        >
-
-          <Button color={"secondary"} sx={{ borderRadius: 1000, color: 'black', backgroundColor: 'secondary.main' }} variant="contained" onClick={() => signIn("azure-ad")}>Ingresar</Button>
-
-        </Box>
-
-
-      </Stack>
-
-    </Box>
-  )
+ 
 }
